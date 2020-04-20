4657c88fdb0df320a69631181a49e08d4ace4b3e
limuyang <limuyang2@163.com>
2020-04-21 00:35:55 +0800

    Modify the code

---
+++ app/src/main/java/com/scwang/refreshlayout/App.java
@@ -0,0 +1,48 @@
+package com.scwang.refreshlayout;
+
+import android.app.Application;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.scwang.refreshlayout.util.DynamicTimeFormat;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ *
+ * Created by scwang on 2017/6/11.
+ */
+public class App extends Application {
+
+    static {
+        //启用矢量图兼容
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+        //设置全局默认配置（优先级最低，会被其他设置覆盖）
+        SmartRefreshLayout.setDefaultRefreshInitializer((context, layout) -> {
+            //全局设置（优先级最低）
+            layout.setEnableAutoLoadMore(true);
+            layout.setEnableOverScrollDrag(false);
+            layout.setEnableOverScrollBounce(true);
+            layout.setEnableLoadMoreWhenContentNotFull(true);
+            layout.setEnableScrollContentWhenRefreshed(true);
+            layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);
+        });
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator((context, layout) -> {
+            //全局设置主题颜色（优先级第二低，可以覆盖 DefaultRefreshInitializer 的配置，与下面的ClassicsHeader绑定）
+            return new ClassicsHeader(context).setTimeFormat(new DynamicTimeFormat("更新于 %s"));
+        });
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+
+}
